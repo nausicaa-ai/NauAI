32c4c3fdc3114d75c1605db45acf344a56deec84
Ron <rsamemiya@gmail.com>
2023-04-03 22:43:09 -0700

    added initial files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import './Components/Login/Login.js';
+import Login from './Components/Login/Login.js';
+
+function App() {
+  return (
+    <Login/>
+  );
+}
+
+export default App;
---
+++ src/Components/Login/Login.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import './Login.css';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Perform login logic here, e.g. send email and password to server
+    console.log('Logged in with email:', email, 'and password:', password);
+  };
+
+  return (
+    <div className="login-container">
+      <h1 className="login-title">Log in to access Nausicaa AI</h1>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email"></label>
+          <input
+            className="login-input"
+            type="email"
+            id="email"
+            value={email}
+            placeholder="Email Address"
+            onChange={handleEmailChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password"></label>
+          <input
+            className="login-input"
+            type="password"
+            id="password"
+            value={password}
+            placeholder="Password"
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <div>
+          <button className="login-submit" type="submit">
+            Log in
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
