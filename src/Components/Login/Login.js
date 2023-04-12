0429fa1200e1e3c6ab8b81a7b6d50892c1b5aea9
Ron <rsamemiya@gmail.com>
2023-04-11 21:11:14 -0700

    edited login.css file and login.js file

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -21,44 +21,56 @@ function Login() {
   };
 
   return (
-    <div className="login-container">
-      <h1 className="login-title">Log in to access Nausicaa AI</h1>
-      <form className="login-form" onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email"></label>
-          <input
-            className="login-input"
-            type="email"
-            id="email"
-            value={email}
-            placeholder="Email Address"
-            onChange={handleEmailChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="password"></label>
-          <input
-            className="login-input"
-            type="password"
-            id="password"
-            value={password}
-            placeholder="Password"
-            onChange={handlePasswordChange}
-          />
-        </div>
-        <div>
-          // note - In the future we may have to change the Link tag to work asynchronously with actually
+    <>
+      <div className="login-container">
+        <h1 className="login-title">Log in to access Nausicaa AI</h1>
+        <form className="login-form" onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="email"></label>
+            <input
+              className="login-input"
+              type="email"
+              id="email"
+              value={email}
+              placeholder="Email Address"
+              onChange={handleEmailChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="password"></label>
+            <input
+              className="login-input"
+              type="password"
+              id="password"
+              value={password}
+              placeholder="Password"
+              onChange={handlePasswordChange}
+            />
+          </div>
+          <div>
+            {/* note - In the future we may have to change the Link tag to work asynchronously with actually
           logging a user in.  Or we can implement the login submit to redirect a user to the home page upon
           successful login/authentication. ChatGPT can help.
-          https://reactrouter.com/en/main/components/link read this for clarification
-          <Link to="/">
-            <button className="login-submit" type="submit">
-              Log in
-            </button>
-          </Link>
+          https://reactrouter.com/en/main/components/link read this for clarification */}
+            <Link to="/">
+              <button className="login-submit" type="submit">
+                Log in
+              </button>
+            </Link>
+          </div>
+        </form>
+
+        <div className="line-text">
+          <hr className="line" />
+          <span className="text">or log in with</span>
+          <hr className="line" />
         </div>
-      </form>
-    </div>
+
+        <div className="google">
+          <button className="login-submit-google">Google</button>
+        </div>
+      </div>
+    </>
   );
 }
 
