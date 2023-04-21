939f2b62546c96ddde765f79d64e8dfa185138ca
Thinh <thinhmai1027@gmail.com>
2023-04-20 20:00:47 -0700

    Candidates graph now shows x-axis and y-axis label, each candidate has a label on top

--- src/Components/Charts/BubbleChart.js
+++ src/Components/Charts/BubbleChart.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Bubble } from 'react-chartjs-2';
+import { Colors } from 'chart.js';
+import 'chartjs-plugin-datalabels';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+import Chart from 'chart.js/auto';
+import 'chartjs-plugin-datalabels';
+
+Chart.register(Colors);
+Chart.register(ChartDataLabels);
+
 
 const BubbleChart = () => {
-
   const data = {
     datasets: [],
   };
 
   // Generate random datasets
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 6; i++) {
     const dataset = {
       label: `Candidate ${i + 1}`,
       title: 'title',
@@ -17,37 +25,55 @@ const BubbleChart = () => {
           x: Math.floor(Math.random() * 101),
           y: Math.floor(Math.random() * 101),
           r: 10,
+          label: `Candidate ${i + 1}`
         },
       ],
-      backgroundColor: `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(
-        Math.random() * 256
-      )}, ${Math.floor(Math.random() * 256)}, 0.2)`,
+      //To randomize colors, but using Colors plugin from chart.js
+      // backgroundColor: `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(
+      //   Math.random() * 256
+      // )}, ${Math.floor(Math.random() * 256)}, 0.2)`,
     };
     data.datasets.push(dataset);
   }
 
   const options = {
+    plugins: {
+      colors: {
+        enabled: true
+      },
+      datalabels: {
+        anchor: "end",
+        align: "top",
+        color: "black",
+        display: function (context) {
+          return context.dataset.data.length > 0; // show labels only for non-empty datasets
+        },
+        formatter: function (value, context) {
+          return context.chart.data.datasets[context.datasetIndex].label; // display dataset label as the value
+        }
+      },
+    },
     scales: {
-      xAxes: [{
-        scaleLabel: {
+      x: {
+        title: {
           display: true,
-          label: 'Skill Match'
+          text: 'Skill Match Score (%)'
         },
         ticks: {
           min: 0,
           max: 100,
         },
-      }],
-      yAxes: [{
-        scaleLabel: {
+      },
+      y: {
+        title: {
           display: true,
-          label: 'Traits Match'
+          text: 'Traits Match Score(%)'
         },
         ticks: {
           min: 0,
           max: 100,
         },
-      }],
+      },
     },
   };
 
